9ac09ddda319c86c069ae62f1a21362de9a778d7
piet <pprotassow@posteo.de>
2024-08-22 18:17:36 +0200

    lifecycle class; tags

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,29 @@
 from django.views.generic import TemplateView
 from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get(self, request, *args, **kwargs):
+        print(request.method)
+        print(request.GET)
+        print(request.COOKIES)
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # by default it returns an empty dictionary
+        context['key1'] = 'value1'
+        context['key2'] = 'value2'
+        context['title'] = 'Hello World'
+        context['first'] = [1, 2, 3]
+        context['second'] = [4, 5, 6]
+        context['today'] = datetime.now()
+        context['notes'] = "<strong>Note:</strong> Always learn something new!"
+        context['js_inject'] = "<script>alert('Hello World')</script>"
+        return context
 
 
 class AboutPageView(TemplateView):
@@ -12,11 +31,17 @@ class AboutPageView(TemplateView):
 
 def home_func_view(request):
     print(request.method) # GET
+    print(request.GET) # GET 
     print(request.COOKIES)
     context = {
         'key1': 'value1',
         'key2': 'value2',
         'title': 'Hello World',
+        'first': [1, 2, 3],
+        'second': [4, 5, 6],
+        'today': datetime.now(),
+        'notes': "<strong>Note:</strong> Always learn something new!",
+        'js_inject':"<script>alert('Hello World')</script>"
     }
 
     return render(request, 'home2.html', context)
