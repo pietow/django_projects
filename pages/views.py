20b0ba279925665293e4a8a2216aa26644d9cc29
piet <pprotassow@posteo.de>
2024-08-22 12:01:35 +0200

    request and response objects

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseForbidden
+from django.shortcuts import render
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -7,3 +9,26 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+def home_func_view(request):
+    print(request.method) # GET
+    print(request.COOKIES)
+    context = {
+        'key1': 'value1',
+        'key2': 'value2',
+        'title': 'Hello World',
+    }
+
+    return render(request, 'home2.html', context)
+    # response_obj = render(request, 'home2.html', context)
+    # response_obj.write('<h1>Hello</h1>')
+    # return response_obj
+
+    another_response = HttpResponse('<h1>bla</h1>')
+    another_response.write('<div>Hello World</div>')
+    another_response.set_cookie('another_cookie', 'value3')
+    return another_response
+
+
+
+
