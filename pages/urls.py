d7a707bca16e85f0f00eb5c8cd7563edaed08852
piet <pprotassow@posteo.de>
2024-08-27 01:30:46 +0200

    change view namw

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, home_func_view
+from .views import HomePageView2, AboutPageView, home_func_view
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name='home'),
+    path("", HomePageView2.as_view(), name='home'),
     path("about/", AboutPageView.as_view(), name='about'),
     path("home/", home_func_view, name='home2'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -30,9 +30,9 @@ class AboutPageView(TemplateView):
     template_name = "about.html"
 
 def home_func_view(request):
-    print(request.method) # GET
+    # print(request.method) # GET
     print(request.GET) # GET 
-    print(request.COOKIES)
+    # print(request.COOKIES)
     context = {
         'key1': 'value1',
         'key2': 'value2',
@@ -44,15 +44,57 @@ def home_func_view(request):
         'js_inject':"<script>alert('Hello World')</script>"
     }
 
-    return render(request, 'home2.html', context)
-    # response_obj = render(request, 'home2.html', context)
-    # response_obj.write('<h1>Hello</h1>')
-    # return response_obj
-
     another_response = HttpResponse('<h1>bla</h1>')
     another_response.write('<div>Hello World</div>')
-    another_response.set_cookie('another_cookie', 'value3')
+    another_response.write('<div>Hello World</div>')
+    another_response.writelines(['<h1>H</h1>', '<h1>H</h1>',
+    '<p>Hello<span>Bla</span></p>'])
+    another_response.writelines(['<h1>H</h1>', '<h1>H</h1>',
+    '<span>Bla</span>', '<span>Bla</span>'])
     return another_response
+
+
+
+
+def home_func_view(request):
+    print(request.method) # GET
+    print(request.GET) # GET 
+    print(request.COOKIES)
+    context = {
+        'key1': 'value1',
+        'key2': 'value2',
+        'title': 'Hello World',
+    }
+
+
+class HomePageView2(TemplateView):
+    template_name = "home2.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        print(request.method) 
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        cookies_dict = request.COOKIES
+        if cookies_dict.get('my_cookie') == 'my_cookie_value':
+            my_response = HttpResponse('With cookie')
+            my_response.set_cookie('key', 'value')
+            return my_response
+        my_super_response = super().get(request, *args, **kwargs)
+        my_super_response.set_cookie('super_key', 'super_value')
+        return my_super_response
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['key1'] = 'value1'
+        context['key2'] = 'value2'
+        context['title'] = 'Hello World'
+        return context
+
+
+
+
+
 
 
 
