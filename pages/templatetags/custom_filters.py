d8389f1c1fed7ab770ed9734d6777407d8a1ace9
piet <pprotassow@posteo.de>
2024-08-27 15:10:25 +0200

    solution

--- pages/templatetags/custom_filters.py
+++ pages/templatetags/custom_filters.py
@@ -3,5 +3,6 @@ from django import template
 register = template.Library()
 
 @register.filter(name='custom_split')
-def custom_split(???):
-    pass
+def custom_split(s, delimiter=' '):
+    s_list = s.split(delimiter)
+    return s_list
